b72ef8aabc73f3d24b320aab85df9f3d31022da9
Tom Lous <tomlous@gmail.com>
2017-01-12 17:11:33 +0100

    WIP

--- decrypt.py
+++ decrypt.py
@@ -15,7 +15,7 @@ def main():
     for n in range(0, len(cypherText)):
         c = cypherText[n:n+1]
         if c == '?':
-            str += '000000'
+            str += '111111'
         else:
             print "---"
             #print format(ord(c.encode()),'b')
@@ -30,48 +30,6 @@ def main():
     print str
     print  binascii.unhexlify('%x' % int('0b' + str, 2))
 
-    # # set the CBC parts. The first part is the IV
-    # cypherTextIV = cypherText[0].decode('hex')
-    # cypherTextC0 = cypherText[1].decode('hex')
-    #
-    # # define plaintexts
-    # plainText = "Pay Bob 100$"
-    # plainTextTarget = "Pay Bob 500$"
-    #
-    # # define paddings
-    # paddingNum1 = str(len(cypherTextC0) - len(plainText))
-    # padding1 = "".join([paddingNum1] * int(paddingNum1))
-    #
-    # paddingNum2 = str(len(cypherTextC0) - len(plainTextTarget))
-    # padding2 = "".join([paddingNum2] * int(paddingNum2))
-    #
-    # # append to plaintext the paddings
-    # plainText += padding1
-    # plainTextTarget += padding2
-    #
-    # # XOR the plaintext to determine the value to XOR with
-    # xorredPlainText = strxor(plainText, plainTextTarget)
-    #
-    # # Since the decription of c[0] is XORed with IV to retrieve the plaintext xor the IV with the desired mutation
-    # newIV = strxor(xorredPlainText, cypherTextIV)
-    #
-    # # new CBC
-    # print "New CBC\n", newIV.encode('hex'), cypherText[1]
-
-
-
-
-# Output:
-# New CBC
-# 20814804c1767293bd9f1d9cab3bc3e7 ac1e37bfb15599e5f40eef805488281d
-
-
-# xor two strings of different lengths
-# def strxor(a, b):
-#     if len(a) > len(b):
-#         return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a[:len(b)], b)])
-#     else:
-#         return "".join([chr(ord(x) ^ ord(y)) for (x, y) in zip(a, b[:len(a)])])
 
 
 main()
